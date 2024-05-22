579e1ae9dfa72c235dfd0662be1296f2032808fe
ColtonWirgau <ColtonWirgau@gmail.com>
2024-05-22 11:41:30 -0400

    Update Test.js

--- MP Widgets/Test.js
+++ MP Widgets/Test.js
@@ -1,29 +1,22 @@
 const cookies = document.cookie;
 var cookie = cookies.split('; ');
 var locationToken = null;
+const MyCustomWidget = document.getElementById("MyCustomWidget");
+var params = MyCustomWidget.getAttribute("data-params");
+
 
 let i = 0;
 
 while (i < cookie.length) {
     if (cookie[i].startsWith('tbx-ws__selected-location=')) {
-        console.log("Cookie " + i + ": ")
-        //console.log(cookie[i].split('tbx-ws__selected-location=')[1]);
         locationToken = cookie[i].split('tbx-ws__selected-location=')[1];
-        console.log(locationToken);
+        //console.log(locationToken);
+        params = params + "&@CongregationID='" + locationToken + "'";
     }
     i++;
 } 
 
 
-const MyCustomWidget = document.getElementById("MyCustomWidget");
-var params = MyCustomWidget.getAttribute("data-params");
-
-if (locationToken !== null) {
-  params = params + "&@CongregationID='" + locationToken + "'";
-} 
-
-
 MyCustomWidget.setAttribute("data-params", params);
-
 
 document.getElementById("Attributes").innerHTML = params;	
