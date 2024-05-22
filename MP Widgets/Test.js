7470d71146e8b2ca00151173f624a106597dcef5
ColtonWirgau <ColtonWirgau@gmail.com>
2024-05-22 11:43:34 -0400

    Update Test.js

--- MP Widgets/Test.js
+++ MP Widgets/Test.js
@@ -1,6 +1,7 @@
 const cookies = document.cookie;
 var cookie = cookies.split('; ');
 var locationToken = null;
+var selectedCongregationID = null;
 const MyCustomWidget = document.getElementById("MyCustomWidget");
 var params = MyCustomWidget.getAttribute("data-params");
 
@@ -11,7 +12,10 @@ while (i < cookie.length) {
     if (cookie[i].startsWith('tbx-ws__selected-location=')) {
         locationToken = cookie[i].split('tbx-ws__selected-location=')[1];
         //console.log(locationToken);
-        params = params + "&@CongregationID='" + locationToken + "'";
+
+        selectedCongregationID = 1234;
+        
+        params = params + "&@CongregationID='" + selectedCongregationID + "'";
     }
     i++;
 } 
