70f6c0e4c48866514d6a890d1f8451ff295284c0
ColtonWirgau <ColtonWirgau@gmail.com>
2024-05-22 10:32:36 -0400

    Update Test.js

--- MP Widgets/Test.js
+++ MP Widgets/Test.js
@@ -1,2 +1,8 @@
 const tok = localStorage.getItem("mpp-widgets_AuthToken");	 
 document.getElementById("Token").innerHTML = tok;	
+
+
+const MyCustomWidget = document.getElementById("MyCustomWidget");
+const params = MyCustomWidget.getAttribute("data-params");
+
+document.getElementById("Attributes").innerHTML = params;	
