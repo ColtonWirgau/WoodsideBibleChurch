b82f34d7ed1058d952daa89940e857bb123a3366
ColtonWirgau <ColtonWirgau@gmail.com>
2024-05-22 11:26:22 -0400

    Update Test.js

--- MP Widgets/Test.js
+++ MP Widgets/Test.js
@@ -4,7 +4,17 @@ var test = cookies.split(';')
 console.log("Cookies: ");
 console.log(test);
 
+let i = 0;
+
+while (i < test.length) {
+    console.log("Cookie " + i + ": ")
+    console.log(test[i]);
+    i++;
+}
+
+
 var locationToken = document.cookie //localStorage.getItem("tbx-ws__selected-location");	 
+
 
 const MyCustomWidget = document.getElementById("MyCustomWidget");
 var params = MyCustomWidget.getAttribute("data-params");
